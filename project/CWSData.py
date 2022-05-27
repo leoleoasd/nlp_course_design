8cc685a6957624feff8a7a3d6456f7f416698da1
Leo Lu <luyuxuanleo@gmail.com>
2022-05-27 20:08:53 +0800

    cws

--- project/CWSData.py
+++ project/CWSData.py
@@ -3,6 +3,7 @@ from transformers import BertTokenizer
 import pytorch_lightning as pl
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data.dataloader import DataLoader
+from IPython import embed
 
 
 class CWSDataset(torch.utils.data.IterableDataset):
@@ -33,6 +34,7 @@ class CWSModule(pl.LightningDataModule):
         self.val_dataset = None
         self.train_dataset = None
         self.test_dataset = None
+        self.predict_dataset = None
         # self.vocab = None
         self.tokenizer = None
         # self.vocab_size = 0
@@ -47,39 +49,59 @@ class CWSModule(pl.LightningDataModule):
         self.val_dataset = CWSDataset(
             f"data/training/{self.dataset_name}_training.utf8_valid")
         self.test_dataset = CWSDataset(
-            f"data/testing/{self.dataset_name}_testing.utf8")
+            f"data/gold/{self.dataset_name}_test_gold.utf8")
+        self.predict_dataset = CWSDataset(
+            f"data/testing/{self.dataset_name}_test.utf8")
+
+    def predict_dataloader(self):
+        return DataLoader(
+            self.predict_dataset,
+            num_workers=1,
+            collate_fn=self.collate(),
+            batch_size=self.batch_size
+        )
 
     def collate(self):
         def _collate(batch: list[str]):
-            texts = []
             segments = []
+            input_ids = []
+            attention_mask = []
+            token_type_ids = []
             for s in batch:
                 s = s.rstrip().split("  ")
-                r = ""
                 # Prepend a zero for [CLS] token
                 seg = [0]
+                input_id = [self.tokenizer.convert_tokens_to_ids('[CLS]')]
                 for i in s:
                     for j in i:
-                        r += j
-                    seg += [0 for _ in range(len(i))]
+                        input_id.append(
+                            self.tokenizer.convert_tokens_to_ids(j)
+                        )
+                        seg.append(0)
                     seg[-1] = 1
-                texts.append(r)
+                input_id.append(self.tokenizer.convert_tokens_to_ids('[SEP]'))
+                input_id = torch.LongTensor(input_id)
+                input_ids.append(input_id)
+                attention_mask.append(torch.ones_like(input_id))
+                token_type_ids.append(torch.zeros_like(input_id))
+
+                seg.append(0)
                 segments.append(torch.LongTensor(seg))
             segments = pad_sequence(segments, True)
+            input_ids = pad_sequence(input_ids, True, self.tokenizer.convert_tokens_to_ids('[PAD]'))
+            attention_mask = pad_sequence(attention_mask, True)
+            token_type_ids = pad_sequence(token_type_ids, True)
             segments = segments[:, :512]
-
-            tokenized = self.tokenizer(text=texts,
-                                       return_tensors="pt",
-                                       max_length=512,
-                                       padding=True,
-                                       truncation="only_first")
+            input_ids = input_ids[:, :512]
+            attention_mask = attention_mask[:, :512]
+            token_type_ids = token_type_ids[:, :512]
             return ((
-                        tokenized.input_ids,
-                        tokenized.attention_mask,
-                        tokenized.token_type_ids,
+                        input_ids,
+                        attention_mask,
+                        token_type_ids,
                     ),
                     segments,
-                    # batch
+                    [i.rstrip() for i in batch]
                     )
 
         return _collate
--- project/cws.py
+++ project/cws.py
@@ -1,5 +1,8 @@
 from argparse import ArgumentParser
+from typing import Any
+
 import torch
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from torch import nn
 import torch.nn.functional as F
 from pytorch_lightning.loggers import WandbLogger
@@ -10,80 +13,64 @@ from transformers import BertModel
 
 class LitAutoEncoder(pl.LightningModule):
 
-    def __init__(self):
+    def __init__(self, args = None):
         super().__init__()
+        self.lr = args.lr if args is not None else 1e-5
         self.bert = BertModel.from_pretrained('bert-base-chinese')
-        self.mlp = nn.Linear(768, 2)
+        self.mlp = nn.Sequential(
+            nn.Linear(768, 50),
+            nn.Tanh(),
+            nn.Linear(50, 2),
+        )
 
-    def forward(self, x):
+    def forward(self, batch):
+        x, y, _ = batch
         input_ids, attention_mask, token_type_ids = x
-        out = self.bert(input_ids, attention_mask, token_type_ids).last_hidden_state
-        return F.logsigmoid(self.mlp(out))
-
-    def training_step(self, batch, batch_idx):
-        x, y = batch
-        # [32, 512, 2]
-        y1 = self.forward(x)
-        y1 = y1[:, :y.size(1), :]
-        # y: [32, xxx]
-        # flat
-        embed()
-        y1 = y1.reshape(-1, 2)
+        embedding = self.bert(input_ids, attention_mask, token_type_ids).last_hidden_state
+        all_score = F.log_softmax(self.mlp(embedding), dim=-1)
+        score = all_score[:, :y.size(1), :]
+        score = score.reshape(-1, 2)
+        attention_mask = attention_mask.reshape(-1)
         y = y.reshape(-1)
-        embed()
-        loss = F.nll_loss(y1, y)
-        out = torch.max(y1, dim=-1)[1]
+        if y.size(0) != score.size(0):
+            embed()
+        loss: torch.Tensor = F.nll_loss(score, y, reduction='none')
+        loss.masked_fill_(attention_mask == 0, 0)
+        loss = loss.mean()
+        out = torch.logical_and(torch.max(score, dim=-1)[1], attention_mask)
         corr = torch.sum(out.logical_and(y))
         prec = corr / torch.sum(out)
         recall = corr / torch.sum(y)
-        f1 = 2 * prec * recall /(prec + recall)
+        f1 = 2 * prec * recall / (prec + recall)
+        return loss, prec, recall, f1, torch.max(all_score, dim=-1)[1]
+
+    def training_step(self, batch, batch_idx):
+        loss, prec, recall, f1, _ = self.forward(batch)
         self.log("train/prec", prec)
         self.log("train/recall", recall)
         self.log("train/f1", f1)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        x, y = batch
-        # [32, 512, 2]
-        y1 = self.forward(x)
-        y1 = y1[:, :y.size(1), :]
-        # y: [32, xxx]
-        # flat
-        y1 = y1.reshape(-1, 2)
-        y = y.reshape(-1)
-        loss = F.nll_loss(y1, y)
-        out = torch.max(y1, dim=-1)[1]
-        corr = torch.sum(out.logical_and(y))
-        prec = corr / torch.sum(out)
-        recall = corr / torch.sum(y)
-        f1 = 2 * prec * recall /(prec + recall)
+        loss, prec, recall, f1, _ = self.forward(batch)
         self.log("valid/prec", prec)
         self.log("valid/recall", recall)
         self.log("valid/f1", f1)
         return loss
 
     def test_step(self, batch, batch_idx):
-        x, y = batch
-        # [32, 512, 2]
-        y1 = self.forward(x)
-        y1 = y1[:, :y.size(1), :]
-        # y: [32, xxx]
-        # flat
-        y1 = y1.reshape(-1, 2)
-        y = y.reshape(-1)
-        loss = F.nll_loss(y1, y)
-        out = torch.max(y1, dim=-1)[1]
-        corr = torch.sum(out.logical_and(y))
-        prec = corr / torch.sum(out)
-        recall = corr / torch.sum(y)
-        f1 = 2 * prec * recall /(prec + recall)
+        loss, prec, recall, f1, _ = self.forward(batch)
         self.log("test/prec", prec)
         self.log("test/recall", recall)
         self.log("test/f1", f1)
         return loss
 
+    def predict_step(self, batch, batch_idx, _ = 0) -> Any:
+        loss, prec, recall, f1, out = self.forward(batch)
+        return out, batch[2]
+
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
         return optimizer
 
 
@@ -96,6 +83,11 @@ def cli_main():
     parser = ArgumentParser()
     parser.add_argument('--batch_size', default=32, type=int)
     parser.add_argument('--dataset_name', type=str, default='msr')
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--not_train', dest='train', default=True, action='store_false')
+    parser.add_argument('--predict', dest='predict', default=False, action='store_true')
+    parser.add_argument('--not_test', dest='test', default=True, action='store_false')
+    parser.add_argument('--test_ckpt_path', type=str, default=None)
     parser = pl.Trainer.add_argparse_args(parser)
     args = parser.parse_args()
 
@@ -104,20 +96,53 @@ def cli_main():
     # ------------
     # model
     # ------------
-    model = LitAutoEncoder()
 
     # ------------
     # training
     # ------------
-    logger = WandbLogger(project="nlp_course_exp")
-    trainer: pl.Trainer = pl.Trainer.from_argparse_args(args, logger=logger)
-    trainer.fit(model, data)
+    logger = WandbLogger(project="nlp_course_exp", log_model='all')
+    checkpoint_callback = ModelCheckpoint(save_top_k=2,
+                                          monitor="valid/f1",
+                                          mode='max',
+                                          filename="cws-e{epoch:02d}-{step}-f1{valid/f1:.4f}",
+                                          save_on_train_epoch_end=True,
+                                          auto_insert_metric_name=False,
+                                          )
+    trainer: pl.Trainer = pl.Trainer.from_argparse_args(args, callbacks=[
+        checkpoint_callback,
+        EarlyStopping(monitor="valid/f1", mode="max")
+    ], logger=logger, log_every_n_steps=1)
 
-    # ------------
-    # testing
-    # ------------
-    result = trainer.test(datamodule=data)
-    print(result)
+    if args.train:
+        model = LitAutoEncoder(args)
+        trainer.fit(model, data)
+    else:
+        model = LitAutoEncoder.load_from_checkpoint(args.test_ckpt_path)
+    if args.test:
+        result = trainer.test(model=model, datamodule=data)
+        print(result)
+    if args.predict:
+        result = trainer.predict(model=model, datamodule=data)
+        # print(result)
+        embed()
+        outs = []
+        for res in result:
+            scores = res[0][:, 1:-1]
+            text = res[1]
+            for s, t in zip(scores, text):
+                out_text = ""
+                for i, c in zip(s, t):
+                    out_text += c
+                    if i:
+                        out_text += "  "
+                if out_text.endswith("。  "):
+                    out_text = out_text[:-3]
+                outs.append(out_text)
+        with open("out.txt", "wb") as f:
+            for o in outs:
+                f.write(o.encode("utf-8") + b"\r\n")
+
+        embed()
 
 
 if __name__ == '__main__':
